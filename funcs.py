415d921f4a455e51c5b18dfd6b7b08d8656788f1
itRianone <timokhindimitry@gmail.com>
2021-04-24 23:42:46 +0300

    Language selector added(inline keyboard)

--- funcs.py
+++ funcs.py
@@ -7,11 +7,12 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def get_page():
-    wiki_wiki = wikipediaapi.Wikipedia('en')
-    url = "https://en.wikipedia.org/wiki/Special:Random"
+def get_page(language):
+    wiki_wiki = wikipediaapi.Wikipedia(str(language))
+    url = f"https://{language}.wikipedia.org/wiki/Special:Random"
     html = requests.get(url)
     soup = BeautifulSoup(html.content, 'lxml')
     title = soup.find(class_="firstHeading").text
     page = wiki_wiki.page(title) 
     return f'{title}\n{page.fullurl}'
+
--- script.py
+++ script.py
@@ -2,11 +2,10 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.dispatcher import FSMContext
 from funcs import get_page
-#from PGDB import db, cursor
+#from states import MyStates
 from aiogram.types import ReplyKeyboardRemove, \
     ReplyKeyboardMarkup, KeyboardButton, \
     InlineKeyboardMarkup, InlineKeyboardButton
-#from states import MyStates
 
 
 token = '1728839487:AAEmQMeRNhJXWyAoGs9sr8w1IL3yDbEnQkA'
@@ -29,10 +28,25 @@ async def welcome(message: types.Message):
 
 @dp.message_handler(content_types=['text'])
 async def main(message):
-
+    language_button_ru = InlineKeyboardButton('Русский', callback_data='ru')
+    language_button_en = InlineKeyboardButton('Английский', callback_data='en')
+    set_language_keyboard = InlineKeyboardMarkup().add(
+        language_button_ru, language_button_en)
     if message.text == 'Получить рандомную статью':
 
-        await bot.send_message(message.chat.id, get_page())
+        await message.reply('Какой язык должен быть у статьи?', reply_markup=set_language_keyboard)
+
+
+@dp.callback_query_handler(lambda c: c.data == 'ru')
+async def process_callback_for_rulang(callback_query: types.CallbackQuery):
+    #await bot.answer_callback_query(callback_query.id)
+    await bot.send_message(callback_query.from_user.id, get_page('ru'))
+
+
+@dp.callback_query_handler(lambda c: c.data == 'en')
+async def process_callback_for_enlang(callback_query: types.CallbackQuery):
+    #await bot.answer_callback_query(callback_query.id)
+    await bot.send_message(callback_query.from_user.id, get_page('en'))
 
 
 if __name__ == "__main__":
