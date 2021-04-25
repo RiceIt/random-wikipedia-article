82947fe5cd643cc9f20c41934615056282d6fbd5
yusupal1ev <yusupal1ev@yandex.ru>
2021-04-25 10:30:17 +0300

    Fix unicode in URL

--- funcs.py
+++ funcs.py
@@ -1,5 +1,6 @@
 import wikipediaapi
 import requests
+import urllib.parse
 from bs4 import BeautifulSoup
 
 
@@ -9,6 +10,9 @@ def get_page(language):
     html = requests.get(url)
     soup = BeautifulSoup(html.content, 'lxml')
     title = soup.find(class_="firstHeading").text
-    page = wiki_wiki.page(title) 
-    return f'{title}\n{page.fullurl}'
+    page = wiki_wiki.page(title)
+    page_url = page.fullurl
+    if language == 'ru':
+        page_url = urllib.parse.unquote(page_url)
+    return f'{title}\n{page_url}'
 
